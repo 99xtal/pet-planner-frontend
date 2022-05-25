f35b8812090d850b4c1e4619453d5080fa033157
Joe <95554565+joerybarczyk@users.noreply.github.com>
2022-05-25 11:26:35 -0400

    Add MealCountdown functionality to Diet Widget + Add react-countdown-hook to project deps + Add nearest meal calculation to DietInfoDisplay component + Add "time left" calculation and time formatting to MealCountdown

--- src/components/DietWidget/DietInfoDisplay.jsx
+++ src/components/DietWidget/DietInfoDisplay.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import MealCountdown from "./MealCountdown";
 
-const DietInfoDisplay = ({ meals, nearestMeal }) => {
+const DietInfoDisplay = ({ meals }) => {
+  const [nearestMeal, setNearestMeal] = useState(null);
+
+  useEffect(() => {
+    const today = new Date();
+    const curHours = today.getHours();
+    const curMinutes = today.getMinutes();
+    const curSeconds = today.getSeconds();
+    const curMs = today.getMilliseconds();
+    const todayInMs =
+      curHours * 3600000 + curMinutes * 60000 + curSeconds * 1000 + curMs;
+
+    const mealTimes = meals.map((meal) => {
+      const mealHours = parseInt(meal.time.slice(0, 2));
+      const mealMinutes = parseInt(meal.time.slice(3, 5));
+      const mealSeconds = parseInt(meal.time.slice(6, 8));
+      let mealTimeInMs =
+        mealHours * 3600000 + mealMinutes * 60000 + mealSeconds * 1000;
+
+      if (mealTimeInMs < todayInMs) {
+        mealTimeInMs += 86400000;
+      }
+
+      return [meal, mealTimeInMs];
+    });
+
+    let nearest = [null, 10000000000];
+    mealTimes.forEach((mealTime) => {
+      console.log("Nearest: " + nearest[1]);
+      console.log(JSON.stringify(mealTime[0]));
+      console.log("Mealtime " + mealTime[1]);
+      if (mealTime[1] < nearest[1]) {
+        nearest = mealTime;
+      }
+    });
+
+    setNearestMeal(nearest[0]);
+  }, []);
+
   return (
     <>
       <Row>
@@ -11,7 +49,9 @@ const DietInfoDisplay = ({ meals, nearestMeal }) => {
         </Col>
       </Row>
       <Row>
-        <Col>{/* <MealCountdown nearestMeal={nearestMeal} /> */}</Col>
+        <Col>
+          {nearestMeal !== null && <MealCountdown nearestMeal={nearestMeal} />}
+        </Col>
       </Row>
       <Row>
         <Col>
--- src/components/DietWidget/MealCountdown.jsx
+++ src/components/DietWidget/MealCountdown.jsx
@@ -1,30 +1,46 @@
 import React, { useState, useEffect } from "react";
+import useCountDown from "react-countdown-hook";
 
 const MealCountdown = ({ nearestMeal }) => {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+  const [timeLeft, { start }] = useCountDown(calculateTimeLeft(), 1000);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  });
+    start();
+  }, []);
 
   function calculateTimeLeft() {
     let d = new Date();
-    const currentTimeInSeconds =
-      d.getHours() * 3600 + d.getMinutes() * 60 + d.getSeconds();
-    const mealTimeInSeconds =
-      parseInt(nearestMeal.time.slice(0, 2)) * 3600 +
-      parseInt(nearestMeal.time.slice(3, 5)) * 60 +
-      parseInt(nearestMeal.time.slice(6, 8));
-    const timeDifference = mealTimeInSeconds - currentTimeInSeconds;
+    const currentTimeInMS =
+      d.getHours() * 3600000 + d.getMinutes() * 60000 + d.getSeconds() * 1000;
+    const mealTimeInMS =
+      parseInt(nearestMeal.time.slice(0, 2)) * 3600000 +
+      parseInt(nearestMeal.time.slice(3, 5)) * 60000 +
+      parseInt(nearestMeal.time.slice(6, 8)) * 1000;
+    const timeDifference = mealTimeInMS - currentTimeInMS;
 
     return timeDifference;
   }
 
-  return <></>;
+  function padZeros(num) {
+    return num.toString().padStart(2, "0");
+  }
+
+  function msToTime(ms) {
+    let seconds = Math.floor(ms / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+
+    seconds = seconds % 60;
+    minutes = minutes % 60;
+
+    return `${padZeros(hours)}:${padZeros(minutes)}:${padZeros(seconds)}`;
+  }
+
+  return (
+    <>
+      <p>{msToTime(timeLeft)}</p>
+    </>
+  );
 };
 
 export default MealCountdown;
