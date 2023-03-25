39bf3018bb21e9d541c02a0b64cabdddcb34e389
jrybarczyk <jrybarczyk@exclusiveresorts.com>
2023-03-25 02:37:11 -0400

    types: pets api

---
+++ src/utils/api/services/pets/index.ts
@@ -0,0 +1,31 @@
+import { get, post, patch, destroy } from '../../config';
+
+import type { Pet, PetCategory } from './types';
+
+export function getPets() {
+  return get<Pet[]>('/pets/');
+}
+
+export function getPetById(petId: number) {
+  return get<Pet>(`/pets/${petId}/`);
+}
+
+export function postPet(newPet: Pet) {
+  return post<Pet>('/pets/', newPet);
+}
+
+export function patchPet(petId: number, updatedPet: Partial<Pet>) {
+  return patch<Pet>(`/pets/${petId}/`, updatedPet);
+}
+
+export function deletePet(petId: number) {
+  return destroy(`/pets/${petId}/`);
+}
+
+export function getBreedsByCategory(categoryId: number) {
+  return get(`/pets/breeds/?categoryId=${categoryId}`);
+}
+
+export function getPetCategories() {
+  return get<PetCategory[]>('/pets/categories/');
+}
