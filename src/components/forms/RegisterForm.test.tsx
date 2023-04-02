7cb36dea38ba2e75685b54899a9b0ccf6c767e9a
jrybarczyk <jrybarczyk@exclusiveresorts.com>
2023-04-02 12:20:35 -0400

    add naive 'second password' verification to Registration Form

--- src/components/forms/RegisterForm.test.tsx
+++ src/components/forms/RegisterForm.test.tsx
@@ -21,57 +21,50 @@ const setupTest = () => {
       submitButton: () => getByText('register', { exact: false }),
       usernameInput: () => getByRole('textbox', { name: 'username' }),
       emailInput: () => getByRole('textbox', { name: 'email' }),
-      passwordInput: () => getByPlaceholderText('password', { exact: false }),
+      passwordInput: () => getByPlaceholderText('Password', { exact: true }),
+      secondPasswordInput: () => getByPlaceholderText('Verify Password', { exact: true }),
     }
   };
 };
 
-test('form not submitted if username not set', async () => {
-  const { user, component: { emailInput, passwordInput, submitButton } } = setupTest();
-
-  await user.click(emailInput());
-  await user.keyboard('test@email.com');
-  await user.click(passwordInput());
-  await user.keyboard('password');
-  await user.click(submitButton());
-
-  expect(mockRegisterCallback).toBeCalledTimes(0);
-});
-
-test('form not submitted if password not set', async () => {
-  const { user, component: { usernameInput, emailInput, submitButton } } = setupTest();
+test('form only submitted if all required fields are set', async () => {
+  const { user, component: { usernameInput, emailInput, passwordInput, secondPasswordInput, submitButton } } = setupTest();
 
   await user.click(usernameInput());
   await user.keyboard('username');
+  await user.click(submitButton());
+
   await user.click(emailInput());
-  await user.keyboard('test@email.com');
+  await user.keyboard('test@gmail.com');
   await user.click(submitButton());
-  
-  expect(mockRegisterCallback).toBeCalledTimes(0);
-});
 
-test('form not submitted if email not set', async () => {
-  const { user, component: { usernameInput, passwordInput, submitButton }} = setupTest();
-
-  await user.click(usernameInput());
-  await user.keyboard('username');
   await user.click(passwordInput());
-  await user.keyboard('password');
+  await user.keyboard('p@ssword1!');
   await user.click(submitButton());
-    
-  expect(mockRegisterCallback).toBeCalledTimes(0);
-});
 
-test('form is submitted if all required fields are set', async () => {
-  const { user, component: { usernameInput, passwordInput, emailInput, submitButton }} = setupTest();
-  
-  await user.click(usernameInput());
-  await user.keyboard('username');
-  await user.click(passwordInput());
-  await user.keyboard('password');
-  await user.click(emailInput());
-  await user.keyboard('test@email.com');
+  await user.click(secondPasswordInput());
+  await user.keyboard('p@ssword1!');
   await user.click(submitButton());
-      
+
   expect(mockRegisterCallback).toBeCalledTimes(1);
+});
+
+test('form is not submitted if second password does not match', async () => {
+  const { user, component: { usernameInput, emailInput, passwordInput, secondPasswordInput, submitButton } } = setupTest();
+
+  await user.click(usernameInput());
+  await user.keyboard('test_user');
+
+  await user.click(emailInput());
+  await user.keyboard('testuser@email.com');
+  
+  await user.click(passwordInput());
+  await user.keyboard('s3cuR3p@$$word');
+
+  await user.click(secondPasswordInput());
+  await user.keyboard('password');
+
+  await user.click(submitButton());
+  
+  expect(mockRegisterCallback).toBeCalledTimes(0);
 });
--- src/components/forms/RegisterForm.tsx
+++ src/components/forms/RegisterForm.tsx
@@ -1,36 +1,62 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import { RegistrationForm } from '../../api/auth/types';
+import { RegistrationForm as APIRegistrationForm } from '../../api/auth/types';
 import { SubmitButton } from '../buttons';
 import { FormPasswordInput, FormTextInput } from '../input';
 
 import styles from './RegisterForm.module.scss';
 
 interface Props {
-    handleSubmit: (formValues: RegistrationForm) => Promise<void>;
+    handleSubmit: (formValues: APIRegistrationForm) => Promise<void>;
 }
 
-const RegisterForm: React.FC<Props> = ({ handleSubmit: onSubmit }) => {
-  const { register, handleSubmit, formState: { isSubmitting } } = useForm<RegistrationForm>();
-      
+interface RegisterForm {
+  username: string;
+  email: string;
+  password: string;
+  verify: string
+}
+
+const RegisterForm: React.FC<Props> = ({ handleSubmit }) => {
+  const { register, handleSubmit: handleFormSubmit, formState: { isSubmitting } } = useForm<RegisterForm>();
+  
+  const onSubmit = (formValues: RegisterForm) => {
+    if (formValues.password !== formValues.verify) {
+      return;
+    }
+    handleSubmit({
+      username: formValues.username,
+      password: formValues.password,
+      email: formValues.email,
+      first_name: '',
+      last_name: '',
+    });
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleFormSubmit(onSubmit)}>
       <div className={styles.register__form}>
-        <FormTextInput<RegistrationForm>
+        <FormTextInput<RegisterForm>
           placeholder='Username'
           label='username'
           register={register}
           required
         />
-        <FormTextInput<RegistrationForm>
+        <FormTextInput<RegisterForm>
           placeholder='Email'
           label='email'
           register={register}
           required
         />
-        <FormPasswordInput<RegistrationForm>
+        <FormPasswordInput<RegisterForm>
           placeholder='Password'
           label='password'
+          register={register}
+          required
+        />
+        <FormPasswordInput<RegisterForm>
+          placeholder='Verify Password'
+          label='verify'
           register={register}
           required
         />
