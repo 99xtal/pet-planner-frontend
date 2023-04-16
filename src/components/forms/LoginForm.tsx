b8e1481f8cb5277f78f027776fe1d2cd73fb23cb
Joe Rybarczyk <joe.rybarczyk@me.com>
2023-04-16 13:22:37 -0400

    fix(forms): use RHF `<Controller />` to integrate custom components with form

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 
 import AuthContext from '../../context/AuthContext';
 import { SubmitButton } from '../buttons';
@@ -7,11 +7,12 @@ import type { LoginForm } from '../../api/auth/types';
 
 import styles from './LoginForm.module.scss';
 import { listMissingFields } from '../../utils/forms';
+import { PasswordInput, TextInput } from '../input';
 
 const LoginForm: React.FC = () => {
   const { loginUser } = useContext(AuthContext);
   const [error, setError] = useState(false);
-  const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<LoginForm>();  
+  const { control, handleSubmit, formState: { errors, isSubmitting } } = useForm<LoginForm>();  
 
   const onSubmit = async (formData: LoginForm) => {
     try {
@@ -24,19 +25,17 @@ const LoginForm: React.FC = () => {
 
   return (
     <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-      <input
-        aria-label='username'
-        type="text"
-        placeholder='Username'
-        className={styles.form__input}
-        {...register('username', { required: true })}
+      <Controller
+        name="username"
+        control={control}
+        rules={{ required: true}}
+        render={({ field }) => <TextInput placeholder='Username' aria-label='username' {...field}/>}
       />
-      <input
-        aria-label='password'
-        type='password'
-        placeholder='Password'
-        className={styles.form__input}
-        {...register('password', { required: true })}
+      <Controller 
+        name="password"
+        control={control}
+        rules={{ required: true }}
+        render={({ field }) => <PasswordInput placeholder='Password' aria-label='password' {...field}/>}
       />
       {!error && !!Object.keys(errors).length && <p className={styles.error}>{`Invalid ${listMissingFields(errors).join(' and ')}.`}</p>}
       {error && <p className={styles.error}>Login failed. Incorrect username or password.</p>}
